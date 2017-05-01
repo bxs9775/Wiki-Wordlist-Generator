4d9935778312d05cc174a0e9fb1e1dc73b78c714
bxs9775 <bxs9775@g.rit.edu>
2017-05-01 19:44:30 -0400

    Stubbed out main server code. * calling "npm start" starts a server, but the server does not return a response

---
+++ src/server.js
@@ -0,0 +1,19 @@
+/**
+ * server.js
+ * (C) 2017 by Brian Sandon (bxs9775@rit.edu)
+ * Released under GPL-3.0
+**/
+//Gets Node.js http module
+var http = require("http");
+
+//Stores the port number we are going to be listening to
+var port = process.env.port || 3000;
+
+//Handles a request
+var onRequest = function(request,response){
+  
+}
+
+http.createServer(onRequest).listen(port);
+
+console.log("The server is running (port - " + port + ").");
