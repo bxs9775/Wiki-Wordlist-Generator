e61dcaa196f1ce560cb679e3630c0430bcbff54c
bxs9775 <bxs9775@g.rit.edu>
2017-05-03 16:10:41 -0400

    Server.js now displays index.html

--- src/server.js
+++ src/server.js
@@ -6,16 +6,52 @@
 //Gets Node.js http module
 var http = require("http");
 
+//import fs and querystring
 var fs = require("fs");
+var queryString = require('querystring');
+
+//Index pages
+var index = fs.readFileSync(__dirname + "/../client/index.html");
+var indexCss = fs.readFileSync(__dirname + "/../client/styles/main-page.css");
+var indexScript = fs.readFileSync(__dirname + "/../client/scripts/main-page.js");
 
 //Stores the port number we are going to be listening to
 var port = process.env.port || 3000;
 
+//////////////////////////////Helper functions///////////////////////////////////
+//writes a request with the specified errorcode, message, and type
+var writeMessage = function(response,errCode,msg,type){
+  response.writeHead(errCode, {"Content-Type": type});
+  response.write(msg);
+  response.end();
+};
+
+///////////////////////////////Main code///////////////////////////
 //Handles a request
 var onRequest = function(request,response){
+  var urlChunks = request.url.split("?");
+  var url = urlChunks[0];
+  var params = queryString.parse(urlChunks[1]);
   
-}
+  console.dir(url);
+  
+  switch(url){
+    case "/styles/main-page.css":
+      writeMessage(response,200,indexCss,"text/css");
+      break;
+    case "/scripts/main-page.js":
+      writeMessage(response,200,indexScript,"text/javascript");
+      break;
+    case "/":
+    default:
+      writeMessage(response,200,index,"text/html");
+      break;
+  }
+};
+
+
 
 http.createServer(onRequest).listen(port);
 
 console.log("The server is running (port - " + port + ").");
+
