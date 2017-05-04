f59c0e69942d851b61800b8392bceab2a5b56de5
BRIAN SANDON (RIT Student) <bxs9775@rit.edu>
2017-05-04 10:48:59 -0400

    Catching errors (incomplete)

--- src/fetchAPIs.js
+++ src/fetchAPIs.js
@@ -45,14 +45,19 @@ var fetchMediaWikiList = function(response,url,limit){
   try{
     bot.settings.endpoint = url + "/w/api.php";
     
-    bot.get({ action: "query", list:"allpages", aplimit: limit, apfrom:"0"}).complete(function(list){
+    var request = bot.get({ action: "query", list:"allpages", aplimit: limit, apfrom:"0"})
+    request.complete(function(resp){
       console.log("Fetching from MediaWiki");
       //jsGlobals.write(response,200,JSON.stringify(list),"text\plain");
-      returnListFromJSON(response,list.query.allpages);
+      returnListFromJSON(response,resp.query.allpages);
+    });
+    request.error(function(e){
+      throw e;
     });
   }
   catch(e){
     jsGlobals.write(response,400,e.name + " - " + e.message,"text/plain");
+    //throw e;
   }
 };
 
@@ -68,11 +73,14 @@ var fetchWikiaList = function(response,url,limit){
       limit: limit
     };
     
-    client.get(urlFULL,args,function(data,statusResponse){
+    var request = client.get(urlFULL,args,function(data,statusResponse){
       //console.log(data);
       
       //jsGlobals.write(response,200,JSON.stringify(data),"text\plain");
       returnListFromJSON(response,data.items);
+    });
+    request.on("error",function(e){
+      throw e;
     });
   }
   catch(e){
